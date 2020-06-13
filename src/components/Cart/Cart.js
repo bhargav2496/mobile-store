02d6285465921a41ab84d1fbb14d31747eadca95
bhargav2496 <38598709+bhargav2496@users.noreply.github.com>
2020-06-13 16:28:05 +0530

    new

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,8 @@ import CartColumns from './CartColumns.js';
 import EmptyCart from './EmptyCart.js';
 import {ProductConsumer} from '../../context';
 import CartList from './CartList.js';
+import CartTotals from './CartTotals.js';
+
 export default class Cart extends Component{
     render(){
         return(
@@ -17,6 +19,7 @@ export default class Cart extends Component{
                                 <Title name="your" title="cart"/>
                                 <CartColumns/>
                                 <CartList value={value}/>
+                                <CartTotals value={value}/>
                                 </React.Fragment>
                                 )
                         }
